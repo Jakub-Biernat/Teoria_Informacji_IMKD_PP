a0474f70cad821ed1097611e191e244ba27339ea
Jakub Biernat <k.biernat@vp.pl>
2026-04-16 09:02:46 +0200

    Rollback commit

--- Lab3/main.py
+++ Lab3/main.py
@@ -36,10 +36,38 @@ def entropia_slow(input_text):
 
     return entropy
 
+def entropia_znakow_warunkowa(input_text, order):
+    inputlength = len(input_text)
+
+    #Slownik n-gramow bez zliczania, do pradopodobienstwa lacznego
+    joint_n_grams = defaultdict(int)
+    #Slownik n-gramow, zliczanie ile razy po danym ciagu znakow wystepuje inny znak
+    n_grams = defaultdict(lambda: defaultdict(int))
+    for i in range(inputlength - order):
+        joint_n_grams[input_text[i : i + order + 1]] += 1
+        n_grams[input_text[i : i + order]][input_text[i + order]] += 1
+
+    #prawdopodobienstwo laczne
+    joint_probs = {}
+    total_joint_n_grams = sum(joint_n_grams.values())
+    for n_gram in joint_n_grams:
+        joint_probs[n_gram] = joint_n_grams[n_gram] / total_joint_n_grams
+
+    #Rozklad prawdopodobienstwa wystapienia kolejnego znaku dla kazdego n-gramu
+    n_grams_probs = {}
+    for first_chars in n_grams:
+        total = sum(n_grams[first_chars].values())
+        n_grams_probs[first_chars] = {
+            next_char: count / total
+            for next_char, count in n_grams[first_chars].items()
+        }
+    print(n_grams_probs)
+    print(joint_probs)
+
 
 
 if __name__ == '__main__':
     input_file = sys.argv[1]
     input_text = open(input_file, "r").read()
-    print(entropia_slow(input_text))
+    print(entropia_znakow_warunkowa(input_text, 1))
 
